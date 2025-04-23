93af3e2da81dff8f73ab6cf54a2d564fd9936e52
Alessio Treglia <al@essio.dev>
2025-04-24 06:57:29 +0800

    Update pkg/hdiutil/hdiutil_test.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- pkg/hdiutil/hdiutil_test.go
+++ pkg/hdiutil/hdiutil_test.go
@@ -40,8 +40,8 @@ func TestInit(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		{"test", args{&configs[0]}, true},
-		{"test", args{&configs[1]}, false},
+{"sandbox_safe_with_volume_size_should_fail", args{&configs[0]}, true},
+ 		{"valid_config_should_succeed",        args{&configs[1]}, false},
 	}
 
 	for _, tt := range tests {
