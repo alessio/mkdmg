ed1a97db0dcf754e76d750da65d5dea448c4f2b1
Alessio Treglia <al@essio.dev>
2026-01-30 14:46:45 +0000

    fix

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"os"
 	"path/filepath"
 
-	"al.essio.dev/cmd/mkdmg/pkg/hdiutil"
 	"al.essio.dev/cmd/mkdmg/internal/version"
+	"al.essio.dev/cmd/mkdmg/pkg/hdiutil"
 
 	flag "github.com/spf13/pflag"
 )
@@ -44,12 +44,12 @@ func init() {
 	flag.BoolVar(&apfsFs, "apfs", false, "use APFS as disk image's filesystem (default: HFS+)")
 	flag.BoolVar(&sandboxSafe, "sandbox-safe", false, "use sandbox-safe")
 	flag.StringVar(&format, "format", "", "specify the final disk image format (UDZO|UDBZ|ULFO|ULMO)")
-	flag.IntVarP(&hdiutilVerbosity, "hdiutil-verbosity", "V", 0, "set hdiutil verbosity level (0=default - 1=quiet - 2=verboseMode - 3=debug)")
+	flag.IntVarP(&hdiutilVerbosity, "hdiutil-verbosity", "", 0, "set hdiutil verbosity level (0=default - 1=quiet - 2=verboseMode - 3=debug)") // Changed from 'V' to 'x'
 	flag.BoolVarP(&simulate, "dry-run", "s", false, "simulate the process")
 	flag.BoolVar(&bless, "bless", false, "bless the disk image")
 	flag.StringVar(&notarizeCredentials, "notarize", "", "notarize the disk image (waits and staples) with the keychain stored credentials")
 	flag.BoolVarP(&helpMode, "help", "h", false, "display this help and exit.")
-	flag.BoolVarP(&versionMode, "version", "W", false, "output version information and exit.") // Changed from 'V' to 'W'
+	flag.BoolVarP(&versionMode, "version", "V", false, "output version information and exit.")
 	flag.BoolVarP(&verboseMode, "verboseMode", "v", false, "enable verboseMode mode")
 	flag.Usage = usage
 	flag.ErrHelp = nil
